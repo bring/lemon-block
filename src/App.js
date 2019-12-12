e541b0eded38529cb417ddf679255d77ede42aa9
anirbansingharoy <anirban.singharoy@tcs.com>
2019-12-12 18:17:07 +0100

    Add New Contract Deployment Flow

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import getWeb3 from "./getWeb3";
 import SimpleStorageABI from "./Contracts/SimpleStorage"
+import SimpleStorageByteCode from "./Contracts/SimpleStorageByteCode"
 import {SIMPLE_STORAGE_CONTACT_ADDRESS} from "./Contants"
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
     const [web3Reference, setWeb3Reference] = useState(null);
     const [accountAddress, setAccountAddress] = useState(null);
     const [updateHistory, setUpdateHistory] = useState([]);
+    const [deployedContractAddressList, setDeployedContractAddressList] = useState([SIMPLE_STORAGE_CONTACT_ADDRESS])
+    const [selectedContractAddress, setSelectedContractAddress] = useState(deployedContractAddressList[0])
+    const [successfulCreationOfContractMsg,setSuccessfulCreationOfContractMsg ] = useState(null)
 
 
     useEffect(() => {
@@ -21,8 +25,8 @@ function App() {
             });
     }, []);
 
-    const sendToSmartContract = async  () => {
-        let simpleStorageContract = new web3Reference.eth.Contract(SimpleStorageABI, SIMPLE_STORAGE_CONTACT_ADDRESS)
+    const sendToSmartContract = async () => {
+        let simpleStorageContract = new web3Reference.eth.Contract(SimpleStorageABI, selectedContractAddress)
         simpleStorageContract
             .methods
             .set(value)
@@ -33,16 +37,18 @@ function App() {
     }
 
     function getStateFromSmartContract() {
-        let simpleStorageContract = new web3Reference.eth.Contract(SimpleStorageABI, SIMPLE_STORAGE_CONTACT_ADDRESS)
+        let simpleStorageContract = new web3Reference.eth.Contract(SimpleStorageABI, selectedContractAddress)
         simpleStorageContract
             .methods
             .get()
             .call()
-            .then((data) => setValueFromSmartContract(data) )
+            .then((data) => {
+                setValueFromSmartContract(data)
+            })
     }
 
     function getUpdateHistory() {
-        let simpleStorageContract = new web3Reference.eth.Contract(SimpleStorageABI, SIMPLE_STORAGE_CONTACT_ADDRESS)
+        let simpleStorageContract = new web3Reference.eth.Contract(SimpleStorageABI, selectedContractAddress)
         simpleStorageContract
             .getPastEvents('NewValueAssigned', {
                 fromBlock: 0,
@@ -56,53 +62,98 @@ function App() {
                 }
             }).sort((a, b) => b.blockNumber - a.blockNumber)))
     }
+
+    const deployAnotherInstance = () => {
+        setSuccessfulCreationOfContractMsg(null)
+        let simpleStorageContract = new web3Reference.eth.Contract(SimpleStorageABI)
+        let contractData = simpleStorageContract
+            .deploy({data: '0x'+SimpleStorageByteCode.object})
+            .encodeABI()
+
+        web3Reference
+            .eth
+            .sendTransaction({from: accountAddress, data: contractData})
+            .then(tx => {
+                setDeployedContractAddressList([tx.contractAddress].concat(deployedContractAddressList))
+                setSuccessfulCreationOfContractMsg("Contract has been successfully created. Contract Address: "+tx.contractAddress)
+
+            })
+    }
+
     return (
         <div className="flexgrid mlm dib">
-            {web3Reference !== null ? <div>
+            { web3Reference !== null ? <div>
                 <div className={"tc"}><h3>Welcome to the basic demonstration of Smart Contract Interaction.</h3>
-                <h6 className={"mlm"}>For this demonstration, We have created a basic smart contract that allow us to update state of a
-                    variable which is part of the smart contract and get state for the same </h6>
+                    <h6 className={"mlm"}>For this demonstration, We have created a basic smart contract that allow us
+                        to update state of a
+                        variable which is part of the smart contract and get state for the same </h6>
                 </div>
-                <div style={{"marginLeft":"30rem"}} className={"mtl"}>
-                  <input className={"form__control w-20"} type={"text"} onChange={(e) => setValue(e.target.value)} required/>
-                    <button disabled={value === null } className={"btn btn--green mlm"} onClick={() => sendToSmartContract()}> Set Value</button> <span>(Initiate a Transaction towards block chain)</span>
-                    <br/>
-                    <button className={"btn btn--green mlm"} onClick={() => getStateFromSmartContract()}> Get Value</button> <span className={"mlm"}>{valueFromSmartContract}</span>
-                    <br/>
-                  <button className={"btn btn--green mlm"} onClick={() => getUpdateHistory()}> Show update history</button> (Retrieve Event history for a specific event "NewValueAssigned")
-                    <br/>
-                </div>
+                <div style={{"marginLeft": "30rem"}} className={"mtl"}>
 
-                    {updateHistory.length !== 0 ? <table>
-                        <thead>
-                        <tr>
-                            <th>Transaction Reference</th>
-                            <th>Block Number</th>
-                            <th>Value</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {updateHistory.map((update) => {
-                            return <tr>
-                                <td>
-                                    {update.transactionHash}
-                                </td>
-                                <td>
-                                    {update.blockNumber}
-                                </td>
-                                <td>
-                                    {update.value}
-                                </td>
-                            </tr>
+                    {successfulCreationOfContractMsg !== null ? <div className="message message--success mw7 pam mbl">
+                        <div className="measure">
+                            {successfulCreationOfContractMsg}
+                        </div>
+                    </div> : null}
 
+                    Deployed Contracts
+                    <select id="customerSelector" className="form__control"
+                            onChange={(e) => setSelectedContractAddress(e.target.value)}>
+                        {deployedContractAddressList.map((contract) => {
+                            return <option value={contract}> {contract} </option>
                         })}
+                    </select>
 
-                        </tbody>
+                    <input className={"form__control w-20"} type={"text"} onChange={(e) => setValue(e.target.value)}
+                           required/>
+                    <button disabled={value === null} className={"btn btn--green mlm"}
+                            onClick={() => sendToSmartContract()}> Set Value
+                    </button>
+                    <span> (Initiate a Transaction towards block chain)</span>
+                    <br/>
+                    <button className={"btn btn--green mlm"} onClick={() => getStateFromSmartContract()}> Get Value
+                    </button>
+                    <span className={"mlm"}>{valueFromSmartContract}</span>
+                    <br/>
+                    <button className={"btn btn--green mlm"} onClick={() => getUpdateHistory()}> Show update history
+                    </button> (Retrieve Event history for a specific event "NewValueAssigned")
+                    <br/>
 
-                    </table> : null}
+                    <button className={"btn btn--green mlm"} onClick={() => deployAnotherInstance()}> Create New Version
+                    </button> (Initiate a Transaction towards block chain)
+                    <br/>
+                </div>
+
+                {updateHistory.length !== 0 ? <table>
+                    <thead>
+                    <tr>
+                        <th>Transaction Reference</th>
+                        <th>Block Number</th>
+                        <th>Value</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {updateHistory.map((update) => {
+                        return <tr>
+                            <td>
+                                {update.transactionHash}
+                            </td>
+                            <td>
+                                {update.blockNumber}
+                            </td>
+                            <td>
+                                {update.value}
+                            </td>
+                        </tr>
+
+                    })}
+
+                    </tbody>
+
+                </table> : null}
 
 
-                <div >
+                <div>
 
                 </div>
             </div> : <div> Waiting for web3 provider to be loaded ...</div>}
