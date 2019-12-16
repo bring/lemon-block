ba0becaa2f39f58157a37899d1bf5ab98ed9f735
Sean Dutch <seandutch@hotmail.com>
2019-12-16 15:15:30 +0100

    Added addresses etc.

--- src/Contracts/TicketMaster.sol
+++ src/Contracts/TicketMaster.sol
@@ -9,24 +9,20 @@ contract TicketMaster {
         string eventName;
         int price;
         string owner; //TODO: Change type to address later (address of private account)
+        address ownerAddress;
         State state;
     }
 
     int count = 0;
     mapping(int => Ticket) tickets;
 
-    event NewTicket(int id, string eventName, int price, string owner);
-    event TicketSold(int id, string eventName, int price, string owner);
-
-    constructor() public {
-        sellTicket("Konsert 1", 100, "Ole");
-        sellTicket("Konsert 2", 1000, "Per");
-    }
+    event NewTicket(int id, string eventName, int price, string owner, address ownerAddress);
+    event TicketSold(int id, string eventName, int price, string owner, address ownerAddress);
 
     function sellTicket(string memory _eventName, int _price, string memory _owner) public {
         int newId = count++;
-        tickets[newId] = Ticket(newId, _eventName, _price, _owner, State.ForSale);
-        emit NewTicket(newId, _eventName, _price, _owner);
+        tickets[newId] = Ticket(newId, _eventName, _price, _owner, msg.sender, State.ForSale);
+        emit NewTicket(newId, _eventName, _price, _owner, msg.sender);
     }
 
     function getTicket(int id) public view returns (int, string memory, int, string memory, State) {
@@ -43,6 +39,7 @@ contract TicketMaster {
         require(tickets[id].state == State.ForSale);
         tickets[id].state = State.Sold;
         tickets[id].owner = newOwner;
-        emit TicketSold(id, tickets[id].eventName, tickets[id].price, tickets[id].owner);
+        tickets[id].ownerAddress = msg.sender;
+        emit TicketSold(id, tickets[id].eventName, tickets[id].price, tickets[id].owner, msg.sender);
     }
 }
