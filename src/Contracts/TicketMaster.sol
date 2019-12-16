cdb849e6869661f9d8c191e1eb05d82f41948754
Sean Dutch <seandutch@hotmail.com>
2019-12-16 14:51:19 +0100

    new super awesome ticket stuff

--- src/Contracts/TicketMaster.sol
+++ src/Contracts/TicketMaster.sol
@@ -16,17 +16,33 @@ contract TicketMaster {
     mapping(int => Ticket) tickets;
 
     event NewTicket(int id, string eventName, int price, string owner);
+    event TicketSold(int id, string eventName, int price, string owner);
+
+    constructor() public {
+        sellTicket("Konsert 1", 100, "Ole");
+        sellTicket("Konsert 2", 1000, "Per");
+    }
 
     function sellTicket(string memory _eventName, int _price, string memory _owner) public {
         int newId = count++;
-        tickets[newId] = Ticket(newId, _eventName, _price, _owner);
+        tickets[newId] = Ticket(newId, _eventName, _price, _owner, State.ForSale);
+        emit NewTicket(newId, _eventName, _price, _owner);
     }
 
-    function getTickets() public view returns (int) {
-        return tickets[count];
+    function getTicket(int id) public view returns (int, string memory, int, string memory, State) {
+        Ticket memory ticket = tickets[id];
+        return (ticket.id, ticket.eventName, ticket.price, ticket.owner, ticket.state);
+    }
+
+    function getTicketCount() public view returns (int) {
+        return count;
     }
 
     function buyTicket(int id, string memory newOwner) public {
-        //TODO
+        // TODO validate transaction -> sufficient funds etc..
+        require(tickets[id].state == State.ForSale);
+        tickets[id].state = State.Sold;
+        tickets[id].owner = newOwner;
+        emit TicketSold(id, tickets[id].eventName, tickets[id].price, tickets[id].owner);
     }
 }
