e6acf8a11ceace6e66d2522d8afa48b5f26cd45b
aggarwalaman1191 <a.aggarwal@tcs.com>
2019-12-16 14:49:36 +0100

    Adding UI changes for app

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import SimpleStorageApp from './SimpleStorageApp';
 import SimpleDonationApp from './SimpleDonationApp';
+import TicketMaster from './TicketMaster'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<SimpleDonationApp />, document.getElementById('root'));
+ReactDOM.render(<TicketMaster />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
